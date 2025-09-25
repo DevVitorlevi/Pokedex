dce28cd7b9ee26af4aebb48fbe1703be7c6b1980
Vitor Levi <levivitor8@gmail.com>
2025-09-25 20:17:41 -0300

    feat: Responsivizando em telas menores

--- pokedex/src/app/home/page.tsx
+++ pokedex/src/app/home/page.tsx
@@ -3,22 +3,32 @@ import Link from "next/link"
 import Pika from "@/assets/pikachu.png"
 export default function HomePage() {
   return (
-      <div className="bg-gradient-to-b from-[#F5DB13] to-[#F2B807] w-screen h-[calc(100vh-91px)]">
-        <div className="flex items-center justify-between h-full">
-            <div className="w-3xl flex-col space-y-25 ml-40">
-                <h1 className="text-7xl tracking-[1rem]"><span className="font-bold">Find</span> all your  favorite <span className="font-bold">Pokemon</span></h1>
-                <h2 className="text-4xl">You can know the type of Pokemon, its strengths, disadvantages and <br /> abilities</h2>
-                <button className="relative bg-[#73D677]
-               text-black text-2xl font-bold py-3 px-6 rounded-lg
+    <div className="bg-gradient-to-b from-[#F5DB13] to-[#F2B807] w-full min-h-screen">
+      <div className="sm:flex flex-col justify-center items-center">
+        <Image src={Pika} alt="" />
+        <div className="flex flex-col gap-6 items-center justify-center text-center">
+          <h1 className="text-3xl md:text-5xl">
+            <span className="font-bold">Find</span> all your favorite{" "}
+            <span className="font-bold">Pokemon</span>
+          </h1>
+          <h2 className="text-2xl md:text-3xl">
+            You can know the type of Pokemon, its strengths, disadvantages and{" "}
+            <br /> abilities
+          </h2>
+          <button
+            className="relative bg-[#73D677]
+               text-black text-2xl font-bold rounded-lg
                shadow-[0_8px_0_0_#398d3c] 
                hover:shadow-[0_4px_0_0_#398d3c]
                active:translate-y-2 active:shadow-[0_4px_0_0_#398d3c] 
-               transition-all duration-200"><Link href={'/pokedex'}>See Pokemons</Link></button>
-            </div>
-            <div>
-              <Image src={Pika} alt=""></Image>
-            </div>  
+               transition-all duration-200 py-1 px-3 md:py-3 md:px-6"
+          >
+            <Link href="/pokedex">See Pokemons</Link>
+          </button>
         </div>
       </div>
+    </div>
   )
 }
+
+
